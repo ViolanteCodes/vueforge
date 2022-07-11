4534ff7f25b9cdb3588f6bfa9df309d976ab564b
Daniel Kelly <me@danielkelly.io>
2022-07-11 12:01:50 -0500

    hardcode all content into project so that Maria can bring in via butter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  target: "static",
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
-  }
-})
+  },
+});
